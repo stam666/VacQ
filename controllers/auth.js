d6559f11f22e3efbbb153e01293f43e7b39a5909
stam <stamp.chayakorn@gmail.com>
2023-04-06 13:53:10 +0700

    API security

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,32 +47,54 @@ exports.register = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-  const {email, password} = req.body;
+  try {
+    const {email, password} = req.body;
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({success: false, msg: 'Please provide and email and password'});
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({success: false, msg: 'Please provide and email and password'});
+    }
+
+    const user = await User.findOne({email}).select('+password');
+
+    if (!user) {
+      return res.status(400).json({success: false, msg: 'Invalid credentials'});
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({success: false, msg: 'Invalid credentials'});
+    }
+
+    // const token = user.getSignedJwtToken();
+    // res.status(200).json({success: true, token});
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      msg: 'Cannot convert email or password to string',
+    });
   }
-
-  const user = await User.findOne({email}).select('+password');
-
-  if (!user) {
-    return res.status(400).json({success: false, msg: 'Invalid credentials'});
-  }
-
-  const isMatch = await user.matchPassword(password);
-
-  if (!isMatch) {
-    return res.status(400).json({success: false, msg: 'Invalid credentials'});
-  }
-
-  // const token = user.getSignedJwtToken();
-  // res.status(200).json({success: true, token});
-  sendTokenResponse(user, 200, res);
 };
 
 exports.getMe = async (req, res, next) => {
   const user = await User.findById(req.user.id);
   res.status(200).json({success: true, data: user});
+};
+
+//@desc Log user out / clear cookie
+//@route GET /api/v1/auth/logout
+//@access Private
+exports.logout = async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 };
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+
 const connectDB = require('./config/db');
 
 dotenv.config({path: './config/config.env'});
@@ -11,12 +17,21 @@ connectDB();
 const auth = require('./routes/auth');
 const hospitals = require('./routes/hospitals');
 const appointments = require('./routes/appointments');
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 1,
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(limiter);
+app.use(hpp());
 
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/hospitals', hospitals);
